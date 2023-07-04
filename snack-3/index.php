e23c7c1c8625584b2ce52ca0ef0abeaf3cf32a13
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-04 12:18:45 +0200

    created the two arrays (letters & numbers)

---
+++ snack-3/index.php
@@ -0,0 +1,19 @@
+<!-- Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall'altro
+es. [a,b,c,d,e], [1,2,3,4,5] → [a,1,b,2,c,3,d,4,e,5]. -->
+
+<?php
+    $letters = ['a', 'b', 'c', 'd', 'e'];
+    $numbers = ['1', '2', '3', '4', '5'];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Snack 3</title>
+</head>
+<body>
+    
+</body>
+</html>
