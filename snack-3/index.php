ed39a1689e178f1a8bd0238a230f4e3a92613c41
vito-pizzulli <vito.pizzulli.webdev@outlook.it>
2023-07-04 12:33:29 +0200

    snack 3 completed

--- snack-3/index.php
+++ snack-3/index.php
@@ -4,6 +4,15 @@ es. [a,b,c,d,e], [1,2,3,4,5] → [a,1,b,2,c,3,d,4,e,5]. -->
 <?php
     $letters = ['a', 'b', 'c', 'd', 'e'];
     $numbers = ['1', '2', '3', '4', '5'];
+    $mergedArray = [];
+
+    function mergeArrayAlternateElements($mergedArray, $firstArray, $secondArray) {
+        for ($i = 0; $i < count($firstArray); $i++) {
+            array_push($mergedArray, $firstArray[$i], $secondArray[$i]);
+        }
+
+        return $mergedArray;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +23,10 @@ es. [a,b,c,d,e], [1,2,3,4,5] → [a,1,b,2,c,3,d,4,e,5]. -->
     <title>PHP Snack 3</title>
 </head>
 <body>
-    
+    <p>
+        <?php
+            var_dump(mergeArrayAlternateElements($mergedArray, $letters, $numbers));
+        ?>
+    </p>
 </body>
 </html>
